50935b6cc41d7028b1f97bdeca69d8c4e7ef19c5
Bjørn Erik Juel <bjorneju@gmail.com>
2023-03-08 11:00:07 +0100

    Create setup.py

---
+++ tutorial_functions/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="tutorial_functions",
+    version="0.1.0",
+    description="Tutorials for unfolding",
+    url="https://github.com/bjorneju/pyphi_units",
+    author="Bjørn E juel",
+    author_email="bjorneju@gmail.com",
+    license="BSD 2-clause",
+    packages=["units"],
+    install_requires=[
+        "tqdm",
+        "numpy",
+        "pandas",
+        "pyphi",
+        "matplotlib",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
